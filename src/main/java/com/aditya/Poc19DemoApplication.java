2507e801a494d75a3cb893e1f04291e146479d51
Ubuntu <ubuntu@ip-172-31-2-215.ec2.internal>
2026-06-24 21:56:18 +0000

    Initial Spring Boot Project

---
+++ src/main/java/com/aditya/Poc19DemoApplication.java
@@ -0,0 +1,12 @@
+package com.aditya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc19DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Poc19DemoApplication.class, args);
+    }
+}
