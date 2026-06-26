a09fee7473b1c4c424de415b835b8f7285ffacc3
Ubuntu <ubuntu@ip-172-31-2-215.ec2.internal>
2026-06-26 11:14:35 +0000

    Updated hello  message

--- src/main/java/com/aditya/HelloController.java
+++ src/main/java/com/aditya/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from hello Feature Branch";
+        return "Hello from hello-api Feature Branch";
     }
 }
